fb5715b64d2513e596aa5f5eb906daac0b95bcca
Quilfort Frederiks <qfrederi@f0r2s17.codam.nl>
2022-12-05 15:35:19 +0100

    No Flags | Working command

---
+++ CPP00/ex01/PhoneBook.hpp
@@ -0,0 +1,23 @@
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+# include <iostream>
+# include <string>
+
+class PhoneBook 
+{     
+    public:           
+        int myNum;       
+        std::string myString;  
+};
+
+class Contact
+{
+	public:           
+        int				index;       
+        std::string 	first_name:
+        std::string		last_name;
+		std::string		nickname;
+}; 
+
+#endif
