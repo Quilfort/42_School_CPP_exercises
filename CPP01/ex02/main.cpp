e1ef69ff53a27fd1044a6ae30fd1c92878522a5a
Quilfort Frederiks <qfrederi@f0r2s19.codam.nl>
2022-12-11 16:08:08 +0100

    CPP01 ex02 finished

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int main ()
+{
+    std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "---------------------------------------------------";
+	std::cout << std::endl << "This is memory address" << std::endl;
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	std::cout << "Memory Addres String: " << &string << std::endl;
+	std::cout << "Memory Addres Pointer: " << stringPTR << std::endl; 
+	std::cout << "Memory Addres Reference: " << &stringREF << std::endl;  
+
+	std::cout << "---------------------------------------------------" << std::endl;
+	std::cout << "---------------------------------------------------";
+
+	std::cout << std::endl << "This is the value of string" << std::endl;
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	std::cout << "Value String: " << string << std::endl;
+	std::cout << "Value Pointer: " << *stringPTR << std::endl; 
+	std::cout << "Value Reference: " << stringREF << std::endl;  
+
+	return (0);
+}
