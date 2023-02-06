aaa24d61b9ac8b7c0340c10fec05f890d9157cf1
Quilfort Frederiks <qfrederi@f0r2s17.codam.nl>
2023-02-06 09:14:50 +0100

    CPP06, Start parsing

--- CPP06/ex00/Src/main.cpp
+++ CPP06/ex00/Src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 //ARGUMENT CONTROLEREN OF HET INT ETC IS
 //DOORSTUREN NAAR DE JUISTE FUNCTIE
@@ -15,19 +16,37 @@ int main(int argc, char const *argv[])
           return EXIT_FAILURE;
      }
 
+     if (isalpha(argv[1][0]))
+     {
+          printf("CHAR IS CHAR \n");
+     }
+     else if (isdigit(argv[1][0]) || (argv[1][0] == '-' && isdigit(argv[1][1])))
+     {
+           printf("Char is NUM\n");
+     }
+     else
+          std::cout << "Argument not valid" << std::endl;
+
+
+
+
+
+
+
+
      std::cout << argv[1] << std::endl;
 
 
-     float f = 3.4f;
-     int n = static_cast<int>(f);
+     //float f = 3.4f;
+     //int n = static_cast<int>(f);
 
-     std::cout << f << std::endl;
-     std::cout << n << std::endl;
+     //std::cout << f << std::endl;
+     //std::cout << n << std::endl;
 
-     int test = 123522;
-     float result = static_cast<float>(test);
+     //int test = 123522;
+     //float result = static_cast<float>(test);
 
-     std::cout << result << std::endl;
+     //std::cout << result << std::endl;
    
 
 
