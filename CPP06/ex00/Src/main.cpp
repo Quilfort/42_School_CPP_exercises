5acf4b7f922996d7cd8fcd1c6f3faaffc3d75c39
Quilfort Frederiks <qfrederi@f0r2s17.codam.nl>
2023-02-06 16:26:35 +0100

    CPP06 Going To Use While Loops

--- CPP06/ex00/Src/main.cpp
+++ CPP06/ex00/Src/main.cpp
@@ -7,82 +7,75 @@
 //STATIC CAST? PER CATEGORIE
 // https://cdn.intra.42.fr/pdf/pdf/68807/en.subject.pdf
 
+
+
+
 int main(int argc, char const *argv[])
 {
      
-     if (argc != 2)
-     {
-          std::cout << "Wrong amount of Arguments" << std::endl;
-          return EXIT_FAILURE;
-     }
+	if (argc != 2)
+	{
+		std::cout << "Wrong amount of Arguments" << std::endl;
+		return EXIT_FAILURE;
+	}
+	std::string input = argv[1];
+	if (input.length() == 1)
+	{
+		std::cout << "CHAR IS 1 " << std::endl;
+		std::cout << "FUNCTION FOR CHAR " << std::endl;
+		// Checken if ASCII
+	}	
+	else if (input.find_first_not_of("-0123456789") == std::string::npos)
+	{
+		for (size_t i = 1; i < input.length(); i++)
+		{
+			if (input[i] == '-')
+			{
+				std::cout << "INCORRECT ARGUMENGT" << std::endl;
+				return (EXIT_FAILURE);
+			}
+		}
+		std::cout << "FUNCTION FOR INT " << std::endl;
+	}
+	else if (input[input.length() - 1] == 'f')
+	{
+		for (size_t i = 0; i < input.length() - 1; i++)
+		{
+			if (input[i] == 'f')
+			{
+				std::cout << "INCORRECT ARGUMENGT" << std::endl;
+				return (EXIT_FAILURE);
+			}
+		}
+		std::cout << "FLOAT" << std::endl;
+	}
+	else
+	{ 
+		std::cout << "DOUBLE " << std::endl;
+	}
 
-
-     std::string input = argv[1];
-
-     if (input.length() == 1)
-     {
-          std::cout << "CHAR IS 1 " << std::endl;
-          return (0);
-
-     }
-
-
-
-     //if (isalpha(argv[1][0]))
-     if (input.find_first_not_of("-0123456789.f") != std::string::npos)
-          std::cout << "FUNCTION FOR CHAR " << std::endl;
-     else if (input.find_first_not_of(".f") == std::string::npos)
-          std::cout << "FUNCTION FOR INT " << std::endl;
-     else if (input.find_first_not_of("f") == (std::string::npos -1))
-          std::cout << "FUNCTION FOR FLOAT " << std::endl;
-     else
-          std::cout << "DOUBLE" << std::endl;
+     //else if (input.find_first_not_of("f") == std::string::npos)
+     //     std::cout << "FUNCTION FOR FLOAT " << std::endl;
+     //else
+     //     std::cout << "DOUBLE" << std::endl;
      
-
      // How to f with char
-
-
-
-
-
-
-     //else if (isdigit(argv[1][0]) || (argv[1][0] == '-' && isdigit(argv[1][1])))
+     //if (input.find_first_not_of("-0123456789") == std::string::npos)
      //{
-     //     int i = 0;
-     //     try
+     //     int test = std::atoi(input.c_str());
+     //     int n = static_cast<int>(test);
+
+     //     if (n != 0)
      //     {
-     //          while (argv[1][i] != '\0')
-     //          {
-     //               int num = std::stoi(argv[1]);
-     //               std::cout << "FUNCTION FOR INT " << num << std::endl;
-     //               i++;
-     //          }
+     //          printf("int\n");
      //     }
-     //     catch (std::invalid_argument)
-     //     {
-     //          try
-     //          {
-     //               float floatNum = std::stof(argv[1]);
-     //               std::cout << "FUNCTION FOR FLOAT " << floatNum << std::endl;
-                    
-     //          }
-     //          catch(std::invalid_argument)
-     //          {
-     //               double doubleNum = std::stod(argv[1]);
-     //               std::cout << "FUNCTION FOR Double " << doubleNum << std::endl;
-                    
-     //          }
-     //     }
+     //     else
+     //          printf("no int\n");
+     //     std::cout << test << std::endl;
+     //     std::cout << n << std::endl;
      //}
      //else
-     //     std::cout << "Argument not valid" << std::endl;
-
-
-     //float f = 3.4f;
-     //int n = static_cast<int>(f);
-
-     //std::cout << f << std::endl;
-     //std::cout << n << std::endl;
+     //     printf("no int\n");
 
      //int test = 123522;
      //float result = static_cast<float>(test);
