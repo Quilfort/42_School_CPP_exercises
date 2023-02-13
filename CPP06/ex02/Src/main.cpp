a92d6338fba1c148ef91825f69344975e1e7cb36
Quilfort Frederiks <qfrederi@f0r2s17.codam.nl>
2023-02-13 12:04:48 +0100

    CPP06 ex02 identify

--- CPP06/ex02/Src/main.cpp
+++ CPP06/ex02/Src/main.cpp
@@ -10,13 +10,29 @@ Base * generate(void)
 {
     // Random 0 - 2 A / B / C
     // Return NULL when failed
-    return (new A());
+    //return (new A());
+    //return (new B());
+    return (new C());
+    //return (NULL);
+}
+
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p) != NULL)
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p) != NULL)
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p) != NULL)
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "ERROR" << std::endl;
 }
 
 int main()
 {
     Base* random = generate();
 
-    std::cout << random << std::endl;
+    //std::cout << random << std::endl;
+    identify(random);
 
 }
