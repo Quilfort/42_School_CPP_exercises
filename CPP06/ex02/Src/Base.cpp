60cf746d78e537f9b03b24dbc709720b67ef6f17
Quilfort Frederiks <qfrederi@f0r2s17.codam.nl>
2023-02-13 11:49:38 +0100

    CPP06 ex02 base classes

---
+++ CPP06/ex02/Src/Base.cpp
@@ -0,0 +1,7 @@
+#include "Base.hpp"
+
+// Destructor
+Base::~Base()
+{
+	std::cout << "Destructor called of Base" << std::endl;
+}
--- CPP06/ex02/Src/main.cpp
+++ CPP06/ex02/Src/main.cpp
@@ -1,6 +1,22 @@
 // https://cdn.intra.42.fr/pdf/pdf/68807/en.subject.pdf
+//dynamic cast
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void)
+{
+    // Random 0 - 2 A / B / C
+    // Return NULL when failed
+    return (new A());
+}
 
 int main()
 {
-    
+    Base* random = generate();
+
+    std::cout << random << std::endl;
+
 }
