6a881109e3bc09b065da7c20da2009128fdafb85
Quilfort Frederiks <qfrederi@f0r1s15.codam.nl>
2023-05-06 11:06:56 +0200

    CPP09 ex00 open file

--- CPP09/ex00/Src/main.cpp
+++ CPP09/ex00/Src/main.cpp
@@ -1,8 +1,25 @@
 #include "BitcoinExchange.hpp"
 
-https://cdn.intra.42.fr/pdf/pdf/83098/en.subject.pdf
+//https://cdn.intra.42.fr/pdf/pdf/83098/en.subject.pdf
 
-int main()
+int main(int argc, char *argv[])
 {
-    
+    if (argc != 2)
+    {
+        std::cout << "Error: could not open file." << std::endl;
+        return 1;
+    }
+    //Check if file is available
+    std::string infileName = argv[1];
+	std::ifstream	infile;
+    infile.open(infileName);
+    if (!infile)
+	{
+        std::cout << "Input file does not exist" << std::endl;
+        return (EXIT_FAILURE);
+    }
+
+
+    return 0;
+
 }
---
+++ CPP09/ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,11 @@
+#ifndef BTC_HPP
+# define BTC_HPP
+
+
+# include <iostream>
+# include <algorithm>
+# include <fstream>
+# include <stack>
+# include <limits.h> 
+
+#endif
